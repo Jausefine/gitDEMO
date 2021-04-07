6e086583f39c7d669085629ac0986e4aed674bb2
Jausefine <81740934+Jausefine@users.noreply.github.com>
2021-04-07 14:07:05 +0900

    Add files via upload

---
+++ first.py
@@ -0,0 +1,24 @@
+# first.py
+# first-order linear ODE
+# Dec. 2017 by Kenji Doya
+
+#packages
+
+import numpy as np
+
+# Right-hand-side function of the ODE
+def dynamics(y, t, a, b):
+    """first-order linear ODE: dy/dt = a*y + b"""
+    return(a*y + b)
+
+# Name of the system
+name = 'first'
+
+# A standard initial state
+initial_state = 1
+
+# Default parameters
+parameters = (-0.1, 0)
+
+# For adding to the system list
+name_state_parameters = [name, initial_state, parameters]
